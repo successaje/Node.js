65c6943fab7fdf3ecbcb1a54cbac3054ceafe9e1
Success Aje <73680210+successaje@users.noreply.github.com>
2023-02-16 22:01:03 +0100

    file systems management

--- Legacies/filesystem/files.js
+++ Legacies/filesystem/files.js
@@ -5,5 +5,28 @@ fs.readFile('./docs/blog.txt', (err, data) => {
     if (err) {
         console.log(err)
     }
-    console.log(data);
+    console.log(data.toString());
 })
+
+console.log("Are you there?")
+
+// write
+fs.writeFile('./docs/blog.txt', "Hello World", () => {
+    console.log("file was written")
+})
+
+fs.writeFile("./docs/blog2.txt", "I from Japan", () => {
+    console.log("blog 2 file was written successfully")
+})
+
+// Directories
+
+if (!fs.existsSync("./assets")) {
+
+    fs.mkdir("./assets", (err) => {
+        if (err) {
+            console.log(err)
+        }
+        console.log("created successfully")
+    })
+}
