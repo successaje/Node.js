8ecfa080035942da2042d774e9eb385531f90e80
Success Aje <73680210+successaje@users.noreply.github.com>
2023-02-17 13:41:55 +0100

    streams and buffers, piping

--- Legacies/streams/streams.js
+++ Legacies/streams/streams.js
@@ -3,15 +3,15 @@ const fs = require('fs' )
 const readstream = fs.createReadStream("../filesystem/docs/blog.txt", { encoding : "utf8"})
 const writestream = fs.createWriteStream("../filesystem/docs/blog3.txt")
 
-// readstream.on('data', (chunk) => {
-//     // if (err){
-//     //     console.log(err)
-//     // }
-//     console.log("------- NEW CHUNK ----------")
-//     console.log(chunk)
-//     writestream.write("\n New Chunk \n")
-//     writestream.write(chunk )
-// })
+readstream.on('data', (chunk) => {
+    // if (err){
+    //     console.log(err)
+    // }
+    console.log("------- NEW CHUNK ----------")
+    console.log(chunk)
+    writestream.write("\n New Chunk \n")
+    writestream.write(chunk )
+})
 
 // Piping
 
