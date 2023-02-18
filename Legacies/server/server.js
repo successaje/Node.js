49e822e19d72d29d84f1720766a6d806da537ab9
Success Aje <73680210+successaje@users.noreply.github.com>
2023-02-18 19:13:49 +0100

    request object

--- Legacies/server/server.js
+++ Legacies/server/server.js
@@ -1,9 +1,9 @@
 const http = require("http")
 
 const server = http.createServer((req, res) => {
-    console.log("Request made")
+    console.log(req.url, req.method)
 })
 
 server.listen(8000, "localhost", () => {
     console.log("Listening to request here on port 8000")
-})
+})  
