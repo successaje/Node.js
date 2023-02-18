e6e694620bbed1d2c9ee92cd00def2a2c8645b55
Success Aje <73680210+successaje@users.noreply.github.com>
2023-02-18 19:54:20 +0100

    response object

--- Legacies/server/server.js
+++ Legacies/server/server.js
@@ -2,6 +2,13 @@ const http = require("http")
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
+    // set header content type 
+    res.setHeader("Content-Type", "text/html")
+    res.write('<head><link rel = "stylesheet" href = "#"></head>')
+    res.write("<p>Hello, yarodomo </p>")
+    res.write("<p> Hello, Kaido </p>")
+
+    res.end()
 })
 
 server.listen(8000, "localhost", () => {
