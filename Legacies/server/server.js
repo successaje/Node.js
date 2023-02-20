cc9c85e4d8d5d027d829ef3dc8631f0af4e650d6
Success Aje <73680210+successaje@users.noreply.github.com>
2023-02-20 01:14:54 +0100

    basic routing cont

--- Legacies/server/server.js
+++ Legacies/server/server.js
@@ -1,18 +1,30 @@
 const http = require("http")
-const fs = require(fs);
 const fs = require("fs")
 
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
     // set header content type 
     res.setHeader("Content-Type", "text/html")
+
+    let path = "../views/"
+    switch(req.url){
+        case "/":
+            path += "index.html"
+            break
+        case "/about":
+            path += "about.html";
+            break
+        default:
+            path += "404.html" 
+            break 
+    }
     // res.write('<head><link rel = "stylesheet" href = "#"></head>')
     // res.write("<p>Hello, yarodomo </p>")
     // res.write("<p> Hello, Kaido </p>")
 
     // res.end()
 
-    fs.readFile("./views/index.html", (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err) {
             console.log(err)
             res.end()
