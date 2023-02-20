ce4cef42e13039846482940192c05741ef015b1c
Success Aje <73680210+successaje@users.noreply.github.com>
2023-02-20 01:08:36 +0100

    basic routing

--- Legacies/server/server.js
+++ Legacies/server/server.js
@@ -6,11 +6,22 @@ const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
     // set header content type 
     res.setHeader("Content-Type", "text/html")
-    res.write('<head><link rel = "stylesheet" href = "#"></head>')
-    res.write("<p>Hello, yarodomo </p>")
-    res.write("<p> Hello, Kaido </p>")
+    // res.write('<head><link rel = "stylesheet" href = "#"></head>')
+    // res.write("<p>Hello, yarodomo </p>")
+    // res.write("<p> Hello, Kaido </p>")
 
-    res.end()
+    // res.end()
+
+    fs.readFile("./views/index.html", (err, data) => {
+        if(err) {
+            console.log(err)
+            res.end()
+        } else {
+            res.end(data);
+        }
+    })
+
+
 })
 
 server.listen(8000, "localhost", () => {
